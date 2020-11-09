3f986e2dda80b570aa30541df0721591d90f9da8
seven4x <seven4x@qq.com>
2020-11-09 17:55:05 +0800

    翻译i18n工具

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/agrison/go-tablib v0.0.0-20160310143025-4930582c22ee // indirect
 	github.com/agrison/mxj v0.0.0-20160310142625-1269f8afb3b4 // indirect
 	github.com/alibaba/sentinel-golang v0.6.1
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.18
 	github.com/bndr/gotabulate v1.1.2 // indirect
 	github.com/clbanning/mxj v1.8.5-0.20200714211355-ff02cfb8ea28 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
@@ -26,12 +27,14 @@ require (
 	github.com/go-playground/validator/v10 v10.4.0
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/snappy v0.0.2 // indirect
+	github.com/iancoleman/orderedmap v0.1.0
 	github.com/importcjj/sensitive v0.0.0-20200106142752-42d1c505be7b
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/lib/pq v1.8.0
 	github.com/magiconair/properties v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.4 // indirect
 	github.com/nacos-group/nacos-sdk-go v1.0.0
+	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.5.1
 	github.com/syndtr/goleveldb v1.0.0 // indirect
--- tool/translation/main.go
+++ tool/translation/main.go
@@ -1,5 +1,168 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/alimt"
+	"github.com/iancoleman/orderedmap"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+
+var (
+	client *alimt.Client
+	from   string
+	to     []string
+	file   string
+	output string
+)
+
+type (
+	Req struct {
+		from   string
+		to     string
+		key    string
+		text   string
+		result string
+	}
+)
+
+func InitClient(ak, sk string) {
+	c, err := alimt.NewClientWithAccessKey("cn-hangzhou", ak, sk)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	client = c
+}
+
+/*
+en英，fr法，de德，it意大利，ja日语,pt葡萄牙，ru俄语,es西班牙，ko韩语
+idea -s zh -t en,fr,de,it,ja,pt,ru,ko -f zh-message.json
+trans -s zh -t en,fr,de,it,ja,pt,ru,ko -f zh-cn-message.json  ak sk
+json 解析成 orderedMap 保持原文案顺序
+api请求并发数控制
+*/
 func main() {
+	Execute()
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "trans",
+	Short: "trans",
+	Long:  `trans`,
+	Run: func(cmd *cobra.Command, args []string) {
+		InitClient(args[0], args[1])
+		omap := getMessage(file)
+		for _, t := range to {
+			trans(from, t, omap, output)
+		}
+	},
+}
+
+func getMessage(file string) *orderedmap.OrderedMap {
+	jsonFile, err := os.Open(file)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Successfully Opened %s \n", file)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	//var result map[string]string
+	o := orderedmap.New()
+
+	me := json.Unmarshal(byteValue, &o)
+	if me != nil {
+		println(me.Error())
+	}
+	return o
+}
+
+func trans(from, to string, o *orderedmap.OrderedMap, output string) error {
+	wg := sync.WaitGroup{}
+	var limit = make(chan int, 50)
+	var lock sync.Mutex // map not safe
+	resultMap := orderedmap.New()
+	defer close(limit)
+
+	for _, k := range o.Keys() {
+		v, _ := o.Get(k)
+		wg.Add(1)
+		limit <- 1
+		key := k
+		go func() {
+			req := &Req{from: from, to: to, key: key, text: v.(string)}
+
+			fmt.Printf("%s \t %s \n", req.key, req.text)
+			aliApi(req)
+			fmt.Printf("result:\t %s \n\n", req.result)
+
+			lock.Lock()
+			resultMap.Set(req.key, req.result)
+			lock.Unlock()
+
+			wg.Done()
+			<-limit //api请求完成再从ch取出才能实现并发控制效果
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Printf("%s done!\n", to)
+
+	//因并发调度resultMap非有序的结果
+	orderedMap := orderedmap.New()
+	for _, k := range o.Keys() {
+		v, _ := resultMap.Get(k)
+		orderedMap.Set(k, v.(string))
+	}
+	bytes, err := json.Marshal(orderedMap)
+	if err != nil {
+		println(err.Error())
+	}
+	err = ioutil.WriteFile(output+"/"+to+"-message.json", bytes, 0777)
+	if err != nil {
+		println(err.Error())
+		return err
+	}
+	return nil
+}
+
+func Execute() {
+	rootCmd.PersistentFlags().StringVarP(&from, "source", "s", "zh", "from")
+	//https://github.com/spf13/cobra/issues/661
+	rootCmd.PersistentFlags().StringSliceVarP(&to, "target", "t", []string{"en", "fr"}, "to")
+	rootCmd.PersistentFlags().StringVarP(&file, "file", "f", "zh-message.json", "file")
+	rootCmd.PersistentFlags().StringVarP(&output, "output", "o", ".", "output")
+	rootCmd.MarkFlagRequired("file")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func aliApi(req *Req) {
+
+	request := alimt.CreateTranslateGeneralRequest()
+	request.Scheme = "https"
+
+	request.FormatType = "text"
+	request.SourceLanguage = req.from
+	request.TargetLanguage = req.to
+	request.SourceText = req.text
+
+	response, err := client.TranslateGeneral(request)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	if response == nil {
+		req.result = req.text
+	} else {
+		req.result = response.Data.Translated
+	}
 
 }
