d8e480eddbcd45cdcf532b4e4f7df4ef2eb074ba
seven4x <gri@golang.org>
2021-02-18 12:39:31 +0800

    底部加上 备案号

--- web/ui/craco.config.js
+++ web/ui/craco.config.js
@@ -37,7 +37,7 @@ module.exports = function ({env}) {
                     }
                 },
                 '/api1': {
-                    target: 'http://127.0.0.1:1323/',
+                    target: 'http://127.0.0.1:80/',
                     ws: false, // websocket
                     changeOrigin: true, //是否跨域
                     secure: false,  // 如果是https接口，需要配置这个参数
--- web/ui/src/pages/home/index.tsx
+++ web/ui/src/pages/home/index.tsx
@@ -5,8 +5,16 @@ import styles from './index.module.css'
 function SiteHome(){
     return (
         <div className={styles.container}>
-            <HotTopic/>
+            <div className={styles.content}>
+                <HotTopic/>
+            </div>
+            <div className={styles.beian}>
+                <a href="http://beian.miit.gov.cn/">浙ICP备2021004326号</a>
+                <span className={styles.slogan}>给有价值的信息投票，获得有价值的信息</span>
+            </div>
         </div>
+
+
     )
 }
 
